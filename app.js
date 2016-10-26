83ef375ece0eff7388d3923a0c8397f4d21b0dbf
cphil31 <cecile_philippe@live.fr>
2016-10-26 22:52:16 +0200

    fonnd final modifié

--- app.js
+++ app.js
@@ -1,50 +1,56 @@
    
 $(".restart").hide();
-// window.location.reload()
+
 (function(){
-  "use strict";
+	"use strict";
   // object
   var app = {
 
-		  			  init: function(){
-				      app.listeners();
-				    },
+  	init: function(){
+  		app.listeners();
+  	},
 
-				    listeners:function(){
-				      		$(".verifie").click(app.day);
-							$(".restart").click(app.restart);
-				    },
+  	listeners:function(){
+  		$(".verifie").click(app.day);
+  		$(".restart").click(app.restart);
+  	},
 
-				    day:function(){
-				    	var day=$("#jour").val();
-				    	var month=$("#mois").val();
-				    	var year=$("#annee").val();
-				    	var total= 
-				    	$("#message").html(moment(year+month+day,"YYYYMMDD").format('dddd')).show(".restart");
-  						
-				    	
-						if(day>31){
-						$("#message").html("le jour doit etre compris entre 1 et 31").css({"background-color": "#ff7473", "font-size": "50px"});
-						$("#jour").css("border-color","#ff7473");
-						}
-						if(day<1){
-						$("#message").html("le jour doit etre compris entre 1 et 31").css({"background-color": "#ff7473", "font-size": "50px"});
-						$("#jour").css("border-color","#ff7473");
-						}
-						if(year<0){
-						$("#message").html("L'année doit etre supérieure à 0").css({"background-color": "#ff7473", "font-size": "50px"});
-						$("#annee").css("border-color","#ff7473");
-						}
-						
-						$(".verifie").hide();
-						$(".restart").show();
-						},
-						restart:function(){
-							window.location.reload()
-						},
-		
-			};
+  	day:function(){
+  		var day=$("#jour").val();
+  		var month=$("#mois").val();
+  		var year=$("#annee").val();
+  		var total= 
+  		$(".affjour").html(moment(year+month+day,"YYYYMMDD").format('dddd')).show(".restart");
+      $('body').css('background-color','#9055A2');
+      $(".overlay").hide();
+      $("#jour").hide();
+      $("#mois").hide();
+      $("#annee").hide();
+      
+      
+      
+      if(day>31){
+        $("#message").html("le jour doit etre compris entre 1 et 31").css({"background-color": "#ff7473", "font-size": "50px"});
+        $("#jour").css("border-color","#ff7473");
+  		}
+  		if(day<1){
+  			$("#message").html("le jour doit etre compris entre 1 et 31").css({"background-color": "#ff7473", "font-size": "50px"});
+  			$("#jour").css("border-color","#ff7473");
+  		}
+  		if(year<0){
+  			$("#message").html("L'année doit etre supérieure à 0").css({"background-color": "#ff7473", "font-size": "50px"});
+  			$("#annee").css("border-color","#ff7473");
+  		}
+  		
+  		$(".verifie").hide();
+  		$(".restart").show();
+  	},
+  	restart:function(){
+  		window.location.reload()
+  	},
+  	
+  };
 
-app.init();
+  app.init();
 
 })();
